602db12cb5a02b93937272a62e74053aab649d3c
yashb007 <bansaly37@gmail.com>
2020-06-22 18:12:54 +0530

    apis added

--- admin/controller.js
+++ admin/controller.js
@@ -76,3 +76,155 @@ exports.seeder = function (req, res) {
         });
 }
 
+exports.add = (req, res) => {
+    try {
+        const _b = req.body;
+        Admin.create({
+            email: _b.email,
+            userName: _b.userName,
+            password: bcrypt.hashSync(_b.password, 0)
+        })
+            .then(u => {
+                // mail the email and password
+               // Mailer(_b);
+                res.status(200).json({status: true});
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(400).json({status: false});
+            });
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({status: false});
+    }
+};
+
+exports.delete = (req, res) => {
+    try {
+        const _b = req.body;
+        Admin.destroy({
+            where: {email: _b.email}
+        })
+            .then(u => {
+                res.status(200).json({status: true});
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(400).json({status: false});
+            });
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({status: false});
+    }
+};
+
+exports.get = (req, res) => {
+    const _b = req.body;
+
+    const opts = {where: {}, attributes: {exclude: ['password']}};
+    if (+_b.offset) opts.offset = +_b.offset;
+    if (+_b.limit) opts.limit = +_b.limit;
+    if (_b.keyword) opts.where.userName = {[sequelize.Op.like]: `%${_b.keyword}%`};
+
+    Admin.findAll(opts)
+        .then(u => {
+            if (!u) {
+                res.status(400).json({
+                    status: false,
+                    message: 'user not found'
+                });
+            } else {
+                res.status(200).json({
+                    status: true,
+                    data: u
+                });
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(400).json({status: false});
+        });
+};
+
+
+exports.update = (req, res) => {
+    const _b = req.body;
+    Admin.update({
+        userName: _b.userName,
+        image: _b.image
+    }, {
+        where: {
+            id: req.user.id,
+        }
+    })
+        .then(u => {
+            res.status(200).json({status: true, data: u});
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(400).json({status: false});
+        });
+};
+
+exports.changePassword = (req, res) => {
+    const _b = req.body;
+    if (typeof _b.password !== 'string') {
+        res.status(400).json({
+            status: false,
+            message: 'Password must be a string'
+        });
+    } else {
+        _b.password = bcrypt.hashSync(_b.password, 0);
+        Admin.update({
+            password: _b.password,
+        }, {
+            where: {
+                id: req.user.id,
+            }
+        })
+            .then(u => {
+                res.status(200).json({status: true});
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(400).json({status: false});
+            });
+    }
+};
+
+exports.forgotPassword = (req, res) => {
+    const _b = req.body;
+    if (!_b.email || typeof _b.email !== 'string') {
+        res.status(400).json({
+            status: false,
+            message: 'Email must be a string'
+        });
+    } else {
+        Admin.findOne(
+            {
+                where: {
+                    email: _b.email
+                }
+            }
+        )
+            .then(u => {
+                if (!u) {
+                    res.status(400).json({
+                        status: false,
+                        message: 'email not found'
+                    });
+                } else {
+                    // send email with auth
+                    // send the auth back to /changePassword to reset password
+                //     const auth = Auth.encode(u.id);
+                //     Mailer(auth);
+                //     res.status(200).json({status: true});
+                 res.json("Successfull route")    
+            }
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(400).json({status: false});
+            });
+    }
+};
--- admin/router.js
+++ admin/router.js
@@ -5,6 +5,13 @@ const Controller = require('./controller');
 
 
 router.post('/login', Controller.login);
+router.post('/changePassword',  Controller.changePassword);
+router.post('/forgotPassword', Controller.forgotPassword);
 router.post('/seeder', Controller.seeder);
+router.post('/add',  Controller.add);
+router.post('/delete',  Controller.delete);
+router.get('/get',  Controller.get);
+router.post('/update',  Controller.update);
+
 
 module.exports = router;
