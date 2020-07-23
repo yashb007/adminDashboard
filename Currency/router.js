4a636726d178237e5ef6cd035ded44ad9ef7da61
yashb007 <bansaly37@gmail.com>
2020-07-23 19:39:38 +0530

    change in currency model

--- Currency/router.js
+++ Currency/router.js
@@ -6,8 +6,8 @@ const Controller = require('./controller');
 router.param('currencyId', Controller.getCurrencyById)
 
 router.post('/add', Controller.add);
-router.post('/delete/:countryId', Controller.delete);
-router.post('/edit/:countryId', Controller.edit);
+router.post('/delete/:currencyId', Controller.delete);
+router.post('/edit/:currencyId', Controller.edit);
 router.get('/get',  Controller.get);
 
 module.exports = router;
