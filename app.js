84ab7389926a13d8bd4356867f85e22ac31e663c
Shobhit Maheshwari <shobhitmaheshwari18@gmail.com>
2020-08-12 04:42:51 +0530

    force

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ console.log(port)
     sequelize.connection()
         .authenticate()
         .then(() => {
-            return sequelize.connection().sync({alter: true, force: false})
+            return sequelize.connection().sync({alter: true, force: true})
         })
         .then(() => {
             console.log(process.versions)
