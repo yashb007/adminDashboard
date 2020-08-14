e73933c1f918c42ef3fc3d498eab7ea5f0caa8fd
Anil-Dhakad <sidkirar02@gmail.com>
2020-08-14 18:44:29 +0530

    updated

--- category/model.js
+++ category/model.js
@@ -1,38 +1,43 @@
-
-const Sequelize = require('../services/sequelize.service').connection();
-const sequelize = require('sequelize');
-const Language = require('../Language/model')
-const Media = require('../media/model')
-const Category = Sequelize.define('Category', {
+const Sequelize = require("../services/sequelize.service").connection();
+const sequelize = require("sequelize");
+const Language = require("../Language/model");
+const Media = require("../media/model");
+const { db } = require("../enviornment");
+const Category = Sequelize.define(
+  "Category",
+  {
     id: {
-        type: sequelize.UUID,
-        defaultValue: sequelize.UUIDV4,
-        primaryKey: true
+      type: sequelize.UUID,
+      defaultValue: sequelize.UUIDV4,
+      primaryKey: true,
     },
     name: {
-        type: sequelize.STRING
+      type: sequelize.STRING,
     },
-    Status:{
-        type:sequelize.BOOLEAN,
-        defaultValue:false
+    Status: {
+      type: sequelize.BOOLEAN,
+      defaultValue: false,
     },
-    ParentId:{
-        type: sequelize.STRING,
-        defaultValue:"0"    
-    }
+    ParentId: {
+      type: sequelize.UUID,
+      defaultValue: null,
+    },
+  },
+  {
+    indexes: [{ unique: true, fields: ["name"] }],
+  }
+);
 
-}, {
-    indexes: [{unique: true, fields: ['name']}]
+Category.belongsTo(Language, {
+  constraints: true,
+  onDelete: "CASCADE",
+  through: "LanguageId",
+  foreignKey: "LanguageId",
 });
-
-
-
-Category.belongsTo(Language, { constraints: true, onDelete: 'CASCADE', through :'LanguageId', foreignKey: 'LanguageId' });
 Language.hasMany(Category);
 
 // Category.hasMany(Category);
 // Category.belongsTo(Category, { as: 'parent',  foreignKey: 'ParentId' });
-
 
 Media.hasMany(Category);
 Category.belongsTo(Media);
