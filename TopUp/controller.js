56580416a27df348b1af99687aea2b283a3f6703
Shobhit Maheshwari <shobhitmaheshwari18@gmail.com>
2020-08-17 21:45:28 +0530

    changes

--- TopUp/controller.js
+++ TopUp/controller.js
@@ -5,26 +5,26 @@ const Config = require('../enviornment/index');
 
 
 
-exports.getTopUpById = (req,res,next,id) => {
-    try{
+exports.getTopUpById = (req, res, next, id) => {
+    try {
         TopUp.findOne({
-            where :{
-                id : id
+            where: {
+                id: id
             }
         }).then(top => {
-            if(!top){
-                return res.status(400).json({  
-                    error : "No topup found in db"
+            if (!top) {
+                return res.status(400).json({
+                    error: "No topup found in db"
                 })
             }
             req.profile = top;
-            
+
             next();
         })
     }
-    catch(err) {
+    catch (err) {
         console.error(err);
-        res.status(400).json({status: false});
+        res.status(400).json({ status: false });
     }
 };
 
@@ -33,21 +33,21 @@ exports.add = (req, res) => {
         const _b = req.body;
         TopUp.create({
             Name: _b.Name,
-            Price : _b.Price,
-            Coins:_b.Coins,
-            languageId:_b.LanguageId,
-            MediumId:_b.MediumId
+            Price: _b.Price,
+            Coins: _b.Coins,
+            languageId: _b.LanguageId,
+            MediumId: _b.MediumId
         })
             .then(u => {
-                res.status(200).json({status: true});
+                res.status(200).json({ status: true });
             })
             .catch(err => {
                 console.error(err);
-                res.status(400).json({status: false});
+                res.status(400).json({ status: false });
             });
     } catch (err) {
         console.error(err);
-        res.status(400).json({status: false});
+        res.status(400).json({ status: false });
     }
 };
 
@@ -58,55 +58,56 @@ exports.edit = (req, res) => {
         const _b = req.body;
         TopUp.update({
             Name: _b.Name,
-            Price:_b.Price,
-            Coins:_b.Coins,      
-            languageId:_b.LanguageId,
-            MediumId:_b.MediumId
-      
-        },{
-            where : {
-            id : req.profile.id
-        }})
+            Price: _b.Price,
+            Coins: _b.Coins,
+            languageId: _b.LanguageId,
+            MediumId: _b.MediumId
+
+        }, {
+            where: {
+                id: req.profile.id
+            }
+        })
             .then(u => {
-                res.status(200).json({status: true});
+                res.status(200).json({ status: true });
             })
             .catch(err => {
                 console.error(err);
-                res.status(400).json({status: false});
+                res.status(400).json({ status: false });
             });
     } catch (err) {
         console.error(err);
-        res.status(400).json({status: false});
+        res.status(400).json({ status: false });
     }
 };
 
-exports.delete = (req,res) => {
+exports.delete = (req, res) => {
     const _b = req.body
     try {
         TopUp.destroy({
-            where: {Name: req.profile.Name}
+            where: { Name: req.profile.Name }
         })
             .then(u => {
-                res.status(200).json({status: true});
+                res.status(200).json({ status: true });
             })
             .catch(err => {
                 console.error(err);
-                res.status(400).json({status: false});
+                res.status(400).json({ status: false });
             });
     } catch (err) {
         console.error(err);
-        res.status(400).json({status: false});
+        res.status(400).json({ status: false });
     }
 };
 
-exports.get = (req,res) => {
+exports.get = (req, res) => {
     const _b = req.body;
-    const opts = {where: {}, attributes: {}};
+    const opts = { where: {}, attributes: {} };
     if (_b.topUpId) opts.where.id = _b.topUpId;
     if (_b.LanguageId) opts.where.LanguageId = _b.LanguageId;
     if (+_b.offset) opts.offset = +_b.offset;
     if (+_b.limit) opts.limit = +_b.limit;
-    if (_b.keyword) opts.where.Name = {[sequelize.Op.like]: `%${_b.keyword}%`};
+    if (_b.keyword) opts.where.Name = { [sequelize.Op.like]: `%${_b.keyword}%` };
 
     TopUp.findAll(opts)
         .then(u => {
@@ -124,24 +125,23 @@ exports.get = (req,res) => {
         })
         .catch(err => {
             console.error(err);
-            res.status(400).json({status: false});
+            res.status(400).json({ status: false });
         });
-    }    
+}
 
-    exports.updateStatus= (req,res) => {
-        TopUp.update({
-            Status: !req.profile.Status
-        },{
-            where:{
-                Name:req.profile.Name
-            }
-        }).then(u => {
-            res.status(200).json({status: true, data: u});
-        })
+exports.updateStatus = (req, res) => {
+    TopUp.update({
+        Status: !req.profile.Status
+    }, {
+        where: {
+            Name: req.profile.Name
+        }
+    }).then(u => {
+        res.status(200).json({ status: true, data: u });
+    })
         .catch(err => {
             console.error(err);
-            res.status(400).json({status: false});
+            res.status(400).json({ status: false });
         });
-    }
+}
 
-    
